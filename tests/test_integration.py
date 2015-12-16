2878198fdbb36a7050f92dcf30ce52ec13bd6f50
Wichert Akkerman <wichert@wiggy.net>
2015-12-16 20:38:54 +0100

    Fix Python2.7 compat error.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -46,6 +46,6 @@ def test_secure_view_requires_auth(app):
 
 def test_login(app):
     r = app.get('/login')
-    token = r.json_body['token']
+    token = str(r.json_body['token'])  # Must be str on all Python versions
     r = app.get('/secure', headers={'X-Token': token})
     assert r.unicode_body == 'OK'
