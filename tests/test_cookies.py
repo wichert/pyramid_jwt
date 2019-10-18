de52bf229fbe001073c80b4436f10e3a42aaabc0
Pedro Peixoto <phrfpeixoto@gmail.com>
2019-10-18 15:39:10 -0300

    Using pytest fixtures

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -1,5 +1,7 @@
 import uuid
 
+import pytest
+
 from pyramid.interfaces import IAuthenticationPolicy
 from webob import Request
 from zope.interface.verify import verifyObject
@@ -7,14 +9,23 @@ from zope.interface.verify import verifyObject
 from pyramid_jwt.policy import JWTTokenAuthenticationPolicy
 
 
+@pytest.fixture(scope='module')
+def principal():
+    return str(uuid.uuid4())
+
+
+@pytest.fixture(scope='module')
+def request():
+    return Request.blank('/')
+
+
 def test_interface():
     verifyObject(IAuthenticationPolicy, JWTTokenAuthenticationPolicy('secret'))
 
 
-def test_cookie():
+def test_cookie(request, principal):
     policy = JWTTokenAuthenticationPolicy('secret')
-    request = Request.blank('/')
-    cookie = policy.remember(request, str(uuid.uuid4())).pop()
+    cookie = policy.remember(request, principal).pop()
 
     assert len(cookie) == 2
 
@@ -23,10 +34,9 @@ def test_cookie():
     assert len(cookie) > 0
 
 
-def test_cookie_name():
+def test_cookie_name(request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', cookie_name='auth')
-    request = Request.blank('/')
-    _, cookie = policy.remember(request, str(uuid.uuid4())).pop()
+    _, cookie = policy.remember(request, principal).pop()
 
     name, value = cookie.split('=', 1)
     assert name == 'auth'
@@ -41,20 +51,17 @@ def test_secure_cookie():
     assert '; HttpOnly' in cookie
 
 
-def test_insecure_cookie():
+def test_insecure_cookie(request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', https_only=False)
-    request = Request.blank('/')
-    _, cookie = policy.remember(request, str(uuid.uuid4())).pop()
+    _, cookie = policy.remember(request, principal).pop()
 
     assert '; secure;' not in cookie
     assert '; HttpOnly' in cookie
 
 
-def test_cookie_decode():
+def test_cookie_decode(request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', https_only=False)
-    request = Request.blank('/')
 
-    principal = str(uuid.uuid4())
     header, cookie = policy.remember(request, principal).pop()
     name, value = cookie.split('=', 1)
     request.cookies = {name: value.split(';', 1)[0]}
