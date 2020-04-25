68a3d1486d14c5184ee3e70197cb0e0926b15e11
Pedro Peixoto <phrfpeixoto@gmail.com>
2020-04-25 14:08:39 -0300

    Rename request fixture to comply with new pytest restrictions.

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -15,7 +15,7 @@ def principal():
 
 
 @pytest.fixture(scope='module')
-def request():
+def dummy_request():
     return Request.blank('/')
 
 
@@ -23,9 +23,9 @@ def test_interface():
     verifyObject(IAuthenticationPolicy, JWTTokenAuthenticationPolicy('secret'))
 
 
-def test_cookie(request, principal):
+def test_cookie(dummy_request, principal):
     policy = JWTTokenAuthenticationPolicy('secret')
-    cookie = policy.remember(request, principal).pop()
+    cookie = policy.remember(dummy_request, principal).pop()
 
     assert len(cookie) == 2
 
@@ -34,9 +34,9 @@ def test_cookie(request, principal):
     assert len(cookie) > 0
 
 
-def test_cookie_name(request, principal):
+def test_cookie_name(dummy_request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', cookie_name='auth')
-    _, cookie = policy.remember(request, principal).pop()
+    _, cookie = policy.remember(dummy_request, principal).pop()
 
     name, value = cookie.split('=', 1)
     assert name == 'auth'
@@ -44,38 +44,38 @@ def test_cookie_name(request, principal):
 
 def test_secure_cookie():
     policy = JWTTokenAuthenticationPolicy('secret', https_only=True)
-    request = Request.blank('/')
-    _, cookie = policy.remember(request, str(uuid.uuid4())).pop()
+    dummy_request = Request.blank('/')
+    _, cookie = policy.remember(dummy_request, str(uuid.uuid4())).pop()
 
     assert '; secure;' in cookie
     assert '; HttpOnly' in cookie
 
 
-def test_insecure_cookie(request, principal):
+def test_insecure_cookie(dummy_request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', https_only=False)
-    _, cookie = policy.remember(request, principal).pop()
+    _, cookie = policy.remember(dummy_request, principal).pop()
 
     assert '; secure;' not in cookie
     assert '; HttpOnly' in cookie
 
 
-def test_cookie_decode(request, principal):
+def test_cookie_decode(dummy_request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', https_only=False)
 
-    header, cookie = policy.remember(request, principal).pop()
+    header, cookie = policy.remember(dummy_request, principal).pop()
     name, value = cookie.split('=', 1)
 
     value, _ = value.split(';', 1)
-    request.cookies = {name: value}
+    dummy_request.cookies = {name: value}
 
-    claims = policy.get_claims(request)
+    claims = policy.get_claims(dummy_request)
     assert claims['sub'] == principal
 
 
-def test_cookie_max_age(request, principal):
+def test_cookie_max_age(dummy_request, principal):
     policy = JWTTokenAuthenticationPolicy('secret', cookie_name='auth',
                                           expiration=100)
-    _, cookie = policy.remember(request, principal).pop()
+    _, cookie = policy.remember(dummy_request, principal).pop()
     _, value = cookie.split('=', 1)
 
     _, meta = value.split(';', 1)
@@ -84,16 +84,16 @@ def test_cookie_max_age(request, principal):
 
 
 @pytest.mark.freeze_time
-def test_expired_token(request, principal, freezer):
+def test_expired_token(dummy_request, principal, freezer):
     policy = JWTTokenAuthenticationPolicy('secret', cookie_name='auth',
                                           expiration=1)
-    _, cookie = policy.remember(request, principal).pop()
+    _, cookie = policy.remember(dummy_request, principal).pop()
     name, value = cookie.split('=', 1)
 
     freezer.tick(delta=2)
 
     value, _ = value.split(';', 1)
-    request.cookies = {name: value}
-    claims = policy.get_claims(request)
+    dummy_request.cookies = {name: value}
+    claims = policy.get_claims(dummy_request)
 
     assert claims == {}
